02fb6571d921c9daf542811dfb0e356a5e6f5c7b
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-07-28 23:07:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,3 +9,20 @@ end
 def using_pop(array)
   array.pop 
 end
+
+def pop_with_args(array)
+  
+end
+
+def using_shift(array)
+  array.shift 
+end 
+
+def shift_with_arg(array)
+  
+end 
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+  
