69cc49cbdf2e04d9b90f5e198d8386567e221e89
Derya Tanriverdi <deryatanriverdi_@hotmail.com>
2019-07-28 23:11:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,4 +25,23 @@ end
 def using_concat(arr1, arr2)
   arr1.concat(arr2)
 end
-  
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end  
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, index)
+  array[index].delete
+end
